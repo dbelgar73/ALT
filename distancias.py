d316e754230f100421c234287ece794bf2746a59
guille <ggarper21@gmail.com>
2023-09-29 13:09:50 +0200

    Generacion Secuencia de Operaciones de Edicion

--- distancias.py
+++ distancias.py
@@ -19,7 +19,46 @@ def levenshtein_matriz(x, y, threshold=None):
 
 def levenshtein_edicion(x, y, threshold=None):
     # a partir de la versión levenshtein_matriz
-    return 0,[] # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    lenX, lenY = len(x), len(y)
+    D = np.zeros((lenX + 1, lenY + 1), dtype=np.int)
+    for i in range(1, lenX + 1):
+        D[i][0] = D[i - 1][0] + 1
+    for j in range(1, lenY + 1):
+        D[0][j] = D[0][j - 1] + 1
+        for i in range(1, lenX + 1):
+            D[i][j] = min(
+                D[i - 1][j] + 1,
+                D[i][j - 1] + 1,
+                D[i - 1][j - 1] + (x[i - 1] != y[j - 1]),
+            )
+    # COMPLETAR Y REEMPLAZAR ESTA PARTE
+    '''
+        l : lista de tuplas a devolver.
+
+        Los dos ultimos bucles se recorren en el caso de
+        que se choque contra una pared de la matriz
+    '''
+    l=[]
+    while(i > 0 and j > 0):
+        m = min(D[i-1][j], D[i][j-1], D[i-1][j-1])
+        if m == D[i-1][j-1]:
+            l.append((x[i-1],y[j-1]))
+            i -= 1
+            j -= 1
+        elif D[i+1][j] == m:
+            l.append(("",y[j-1]))
+            i -= 1
+        else:
+            l.append((x[i-1],""))
+            i -= 1
+    while(i > 0):
+        l.append((x[i-1],""))
+        i -= 1
+    while(j > 0):
+        l.append(("",y[j-1]))
+        j-=1
+    l.reverse()
+    return l
 
 def levenshtein_reduccion(x, y, threshold=None):
     # completar versión con reducción coste espacial
